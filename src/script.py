c6df2930dde4b9b6eafffe6ad389d896a7826233
＝°有种爱叫东方不败 <1016068291@qq.com>
2018-06-25 19:19:48 +0800

    第一次提交

--- src/script.py
+++ src/script.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3  
 # -*- coding: utf-8 -*-  
-"""  
- @desc:  
- @author: DYL  
- @contact: chng547835@163.com  
- @site: www.xxxx.com  
- @software: PyCharm  @since:python 3.5.2 on 2016/11/3.10:49  
- """
+
 from conf import setting
 from . import client
 
